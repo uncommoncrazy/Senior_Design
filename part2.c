decf8c3e01fe38eb4a5a2a21955c6e476cb23286
Marco Munoz <marcomunoz94@gmail.com>
2019-11-22 04:48:14 -0500

    codec not streaming

--- part2.c
+++ part2.c
@@ -1,46 +1,64 @@
 
 
 /**
- * part2.c
+ * main.c
  */
-/*Descriptions
- * This codes uses dma to accomplish Double buffering to input and ouput audio while performing DFTs
- *on ready to be processed buffers
+/*
+ *  this code streams from input jack of codec to output jack
  */
 #include<F28x_Project.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "LCDDriver.h"
-#include "ADCDriver.h"
-#include "AIC23.h"
-#include "IODriver.h"
-#include "audioFX.h"
-#include "DFTLib.h"
-Uint16 audioFlags;
-Uint16 audioIsReady;
-Uint16 recordFlag;
-Uint16 playFlag;
+#include "InitAIC23.h"
+#include "OneToOneI2CDriver.h"
+//#define     ADCA_BASE    0x7400
+#define     LEDPins     0x00ff
+#define     nLEDPins    0xffffff00
+#define     PBPins      0x0001C000
+#define     SwitchPins  0x00000f00
+#define     Switch      8
+#define     PB          9
+#define     PU          (PBPins|SwitchPins)
+#define     upperNib    0xf0
+#define     lowerNib    0x0f
+#define     EnableHi    0x0C
+#define     EnableLo    0x08
+#define     RS          0x01
+#define     RSEnableHi  0x0C | RS
+#define     RSEnableLo  0x08 | RS
+#define     LCD_init1   0x33
+#define     LCD_init2   0x32
+#define     Mode        0x28
+#define     DCP         0x0f
+#define     ClearDisp   0x01
+#define     Line2       0xC0
+#define     LCD_addr    0x27
+#define     HighByte    7
+#define     LowByte     9
+#define     SRAM_SizeW  0x1ffff
+#define     inst_Read   3<<8
+#define     inst_Write  2<<8
+#define     left        0
+#define     right       1
 int16 audioIn;
 Uint16 audioOut;
-__interrupt void MSBR_isr(void);
-Uint16 Buttons[3]={0,0,0};
-Uint16  index = 0;
-Uint16 adcSignal = 0;
+Uint16 audioFlags;
+//Uint16  channel = left;
 int16 channel1=0;
+#define audioReady 1
 int16 channel2=0;
-Uint16 chanl=0;
+Uint16 channel=0;
 Uint16  interruptStore=0;
-__interrupt void local_D_INTCH4_ISR(void);
-float freq = 0.0;
-float mag = 0.0;
+__interrupt void MSBR_isr(void);
 int main(void)
 {
-    Uint16 * const I2C_Data = LCDBuffer();
     InitSysCtrl();
-    InitGpio();
-    Init_IO();
-    Init_LCD(I2C_Data);
-    Init_SRAM();
+
+    InitSPIA();
+
+    InitMcBSPb();
+
+    InitAIC23();
     EALLOW;
     DINT;
     InitPieCtrl();
@@ -48,36 +66,46 @@ int main(void)
     IFR = 0x0000;
     InitPieVectTable();
     EALLOW;
-    PieVectTable.DMA_CH6_INT= &local_D_INTCH4_ISR;
-    PieCtrlRegs.PIEIER7.bit.INTx6 = 1;   // Enable PIE Group 7, INT 4 (DMA CH4)
-    IER |= M_INT7;
-    EDIS;
-    DMAInitialize();
-    dma_AudioBuffers();
-    InitSPIA();
-    InitMcBSPb();
-    InitAIC23();
-    generateTables();
-    while(1){
-        switch(audioFlags&DFTflag){
-           case (DFTflag):
-               calculateDFT();
-               freq=findMax(46875.0);
-               mag = getMaxMag();
-               break;
-       }
-    }
-
-
+    PieVectTable.MCBSPB_RX_INT = &MSBR_isr;
+    PieCtrlRegs.PIEIER6.bit.INTx7 = 1;
+    IER |= M_INT6; //Enable group 1 interrupts
+    EINT;
+    while(1);
 }
-__interrupt void local_D_INTCH4_ISR(void)
-{
-    EALLOW;
 
-    PieCtrlRegs.PIEACK.all |= PIEACK_GROUP7; // ACK to receive more interrupts
-                                            // from this PIE group
-    EDIS;
-    GpioDataRegs.GPATOGGLE.bit.GPIO1 = 1;
-    audioFlags|=DFTflag;
-    dma_bufferSwap();
+
+//__interrupt void MSBR_isr(void)
+//{
+//        // right channel transmission
+//       interruptStore=IER;
+//       IER &= M_INT6;
+//          channel1 = McbspbRegs.DRR1.all;
+//          channel2 = McbspbRegs.DRR2.all;
+//          audioIn = channel1;
+//          McbspbRegs.DXR1.all = channel1;
+//          McbspbRegs.DXR2.all = channel1;
+//          audioFlags|=audioReady;
+//          PieCtrlRegs.PIEACK.all |= PIEACK_GROUP6;
+//          IER|=interruptStore;
+//       audioFlags|=audioReady;
+//       PieCtrlRegs.PIEACK.all |= PIEACK_GROUP6;
+//       IER|=interruptStore;
+//}
+__interrupt void MSBR_isr(void)
+{
+        // right channel transmission
+       if(channel)
+       {
+           McbspbRegs.DXR1.all = McbspbRegs.DRR1.all;
+           McbspbRegs.DXR2.all = McbspbRegs.DRR2.all;
+           channel = left;
+       }
+       // left channel transmission
+       else
+       {
+           McbspbRegs.DXR1.all = McbspbRegs.DRR1.all;
+           McbspbRegs.DXR2.all = McbspbRegs.DRR2.all;
+           channel = right;
+       }
+       PieCtrlRegs.PIEACK.all |= PIEACK_GROUP6;
 }
